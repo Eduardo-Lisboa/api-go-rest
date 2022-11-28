d87ff4a2106b870f7c13f92264dc854aefd075c4
Eduardo Henrique Lisboa Alves <henriqueeduardo63@gmail.com>
2022-11-28 16:13:32 -0300

    projeto finalizado

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -14,6 +14,7 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Home page")
 }
 func AllPersonalities(w http.ResponseWriter, r *http.Request) {
+
 	var p []models.Personalidade
 	database.DB.Find(&p)
 	json.NewEncoder(w).Encode(p)
@@ -26,4 +27,30 @@ func OnePersonality(w http.ResponseWriter, r *http.Request) {
 	database.DB.First(&p, id)
 	json.NewEncoder(w).Encode(p)
 
+}
+func CreatePersonality(w http.ResponseWriter, r *http.Request) {
+	var p models.Personalidade
+	json.NewDecoder(r.Body).Decode(&p)
+	database.DB.Create(&p)
+	json.NewEncoder(w).Encode(p)
+}
+
+func DeletePersonality(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var p models.Personalidade
+	database.DB.First(&p, id)
+	database.DB.Delete(&p)
+	json.NewEncoder(w).Encode("Personality deleted")
+}
+func UpdatePersonality(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var p models.Personalidade
+	database.DB.First(&p, id)
+	json.NewDecoder(r.Body).Decode(&p)
+	database.DB.Save(&p)
+	json.NewEncoder(w).Encode(p)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -5,14 +5,20 @@ import (
 	"net/http"
 
 	"github.com/Eduardo-Lisboa/go-res-api/controllers"
+	"github.com/Eduardo-Lisboa/go-res-api/middleware"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentType)
 
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.AllPersonalities).Methods("GET")
 	r.HandleFunc("/api/personalidades/{id}", controllers.OnePersonality).Methods("GET")
-	log.Fatal(http.ListenAndServe(":9090", r))
+	r.HandleFunc("/api/personalidades", controllers.CreatePersonality).Methods("POST")
+	r.HandleFunc("/api/personalidades/{id}", controllers.DeletePersonality).Methods("DELETE")
+	r.HandleFunc("/api/personalidades/{id}", controllers.UpdatePersonality).Methods("PUT")
+	log.Fatal(http.ListenAndServe(":9090", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
